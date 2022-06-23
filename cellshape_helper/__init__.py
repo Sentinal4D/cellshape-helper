3d3721ba0d32f5b0d00101cee6041b38951484b7
DeVriesMatt <matthew.devries.13@gmail.com>
2022-06-23 14:39:17 +0100

    upload packaging

--- cellshape_helper/__init__.py
+++ cellshape_helper/__init__.py
@@ -1,2 +1,4 @@
 from cellshape_helper.conversions import *
 from cellshape_helper.segmentation import *
+
+__version__ == "0.0.0"
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name="cellshape-helper",
-    version="0.0.1",
+    version="0.0.0",
     description="3D shape analysis using deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
