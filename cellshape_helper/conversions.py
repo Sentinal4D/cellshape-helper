b12543d8028b6d90884a2cf97d14aa8b081d1ee2
DeVriesMatt <matthew.devries.13@gmail.com>
2022-06-23 18:54:41 +0100

    tif to mesh

--- cellshape_helper/conversions.py
+++ cellshape_helper/conversions.py
@@ -11,64 +11,34 @@ from pathlib import Path
 
 def tif_to_mesh(tif_directory, save_directory):
     p = Path(tif_directory)
-    files = list(p.glob("**/*.tif"))
+    files = list(p.glob("*.tif"))
     for tif_file in tqdm(files):
         tif_file_path = Path(tif_file)
         img = imread(tif_file)
         vertices, faces, normals, values = marching_cubes(img)
         mesh_obj = pymesh.form_mesh(vertices, faces, values)
-        print(str(tif_directory))
         save_to_mesh_path = save_directory
         create_dir_if_not_exist(save_to_mesh_path)
         split_string = tif_file_path.name.split(".")
         file_name = split_string[0]
         pymesh.save_mesh(save_to_mesh_path + file_name + ".off", mesh_obj)
-        return save_to_mesh_path
 
 
-def mesh_to_pc(mesh_directory, num_points):
+def mesh_to_pc(mesh_directory, num_points, save_dir):
     p = Path(mesh_directory)
-    files = list(p.glob("**/*.off"))
+    files = list(p.glob("*.off"))
     for mesh_file in tqdm(files):
         mesh_file_path = Path(mesh_file)
         data = read_off(mesh_file)
         points = sample_points(data=data, num=num_points)
-        save_to_points_path = str(mesh_directory)[:-1] + "_points"
+        save_to_points_path = save_dir
         create_dir_if_not_exist(save_to_points_path)
         split_string = mesh_file_path.name.split(".")
         file_name = split_string[0]
         cloud = PyntCloud(pd.DataFrame(data=points, columns=["x", "y", "z"]))
         cloud.to_file(save_to_points_path + file_name + ".ply")
-        return points
 
 
-def tif_to_pc_directory(tif_directory, num_points):
-    mesh_directory = tif_to_mesh(tif_directory)
-    mesh_to_pc(mesh_directory, num_points)
-
-
-def tif_to_mesh_directory(image_directory, save_directory):
-    p = Path(image_directory)
-    files = list(p.glob("**/*.tif"))
-    for file in tqdm(files):
-        mesh_obj = tif_to_mesh(file)
-        save_to = save_directory + "/"
-        create_dir_if_not_exist(save_to)
-        pymesh.save_mesh(save_to + file[:-3] + "off", mesh_obj)
-
-
-def mesh_to_pc_directory(mesh_directory, save_directory, num_points):
-    p = Path(mesh_directory)
-    files = list(p.glob("**/*.obj"))
-    for file in tqdm(files):
-        points = mesh_to_pc(obj_file=file, num_points=num_points)
-        save_to = save_directory + "/"
-        create_dir_if_not_exist(save_to)
-        cloud = PyntCloud(pd.DataFrame(data=points, columns=["x", "y", "z"]))
-        cloud.to_file(save_to + file[:-3] + "ply")
-
-
-def tif_to_pc(tif_file, num_points):
-    mesh = tif_to_mesh(tif_file)
-    pc = mesh_to_pc(mesh, num_points)
-    return pc
+def tif_to_pc_directory(tif_directory, save_mesh, save_points, num_points):
+    tif_to_mesh(tif_directory, save_mesh)
+    mesh_to_pc(save_mesh, num_points, save_points)
