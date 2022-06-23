c55afc2acc3d5442cbd140d5b72b087093c042d1
DeVriesMatt <matthew.devries.13@gmail.com>
2022-06-23 17:19:29 +0100

    Bump version: 0.0.4-rc0 → 0.0.5-rc0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [
 
 setup(
     name="cellshape-helper",
-    version="0.0.4-rc0",
+    version="0.0.5-rc0",
     description="3D shape analysis using deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
