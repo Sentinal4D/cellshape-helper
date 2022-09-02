39f6254708caf803772479798699c0ca509c776d
DeVriesMatt <matthew.devries.13@gmail.com>
2022-09-02 17:16:50 +0100

    Bump version: 0.0.17 → 0.0.18-rc0

--- cellshape_helper/__init__.py
+++ cellshape_helper/__init__.py
@@ -2,4 +2,4 @@ from cellshape_helper.conversions import *
 from cellshape_helper.segmentation import *
 from .vendor.pytorch_geometric_files import *
 
-__version__ = "0.0.17"
+__version__ = "0.0.18-rc0"
